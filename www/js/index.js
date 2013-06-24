29f2973e07fc3d6b0b5b62da5067dcbbf705e8ec
simonsafhalter <simon@tvappagency.com>
2013-06-24 13:22:25 +0200

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -45,5 +45,19 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+        
+        var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+
+       scanner.scan(
+          function (result) {
+              alert("We got a barcode\n" +
+                    "Result: " + result.text + "\n" +
+                    "Format: " + result.format + "\n" +
+                    "Cancelled: " + result.cancelled);
+          }, 
+          function (error) {
+              alert("Scanning failed: " + error);
+          }
+       );
     }
 };
